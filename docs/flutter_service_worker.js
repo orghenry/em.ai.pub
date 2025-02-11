ecb0374b4e5dfe4f941debd2ec6186fd214e057c
Henry Moon <hcmoon92@hotmail.com>
2025-02-11 23:04:42 +0900

    Revert "updated"
    This reverts commit 82b95cfa6729ce8225630a3e0ef7bc1f312c13df.

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "96de9019218719064069ac48e6760925
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "4231b011e263e06bd747375d5f9788c2",
+"flutter_bootstrap.js": "68572f2925d81bc9eaf4748760e8d008",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
